1bd83bf8f2fdc0a72e71d9e29368a4fbd9298d5a
wolf-yuan <wolf@wolf-yuan.dev>
2025-01-16 23:30:09 +0800

    chore: use prettier to format code

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: [
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+};
