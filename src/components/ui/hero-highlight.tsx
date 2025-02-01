31c05901d6e94831225855a397d150364736a13e
wolf-yuan <wolf@wolf-yuan.dev>
2025-02-01 14:30:39 +0800

    refactor: migrate to tailwindcss v4

--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -86,7 +86,7 @@ export const Highlight = ({
         display: "inline",
       }}
       className={cn(
-        `relative inline-block bg-gradient-to-r from-indigo-500 to-purple-500 px-1 pb-1 dark:from-indigo-500 dark:to-purple-500`,
+        `relative inline-block bg-linear-to-r from-indigo-500 to-purple-500 px-1 pb-1 dark:from-indigo-500 dark:to-purple-500`,
         className,
       )}
     >
--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -48,13 +48,13 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-card flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-primary p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold font-unifont bg-gradient-to-r from-secondary to-primary text-transparent bg-clip-text">
+              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold font-unifont bg-linear-to-r from-secondary to-primary text-transparent bg-clip-text">
                 {item.title}
               </h3>
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-unifont font-bold bg-gradient-to-r from-secondary to-primary text-transparent bg-clip-text">
+              <h3 className="md:hidden block text-2xl mb-4 text-left font-unifont font-bold bg-linear-to-r from-secondary to-primary text-transparent bg-clip-text">
                 {item.title}
               </h3>
               {item.content}{" "}
@@ -65,14 +65,14 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           style={{
             height: height + "px",
           }}
-          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-0% via-neutral-700 to-transparent to-99%  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
         >
           <motion.div
             style={{
               height: heightTransform,
               opacity: opacityTransform,
             }}
-            className="absolute inset-x-0 top-0  w-[2px] bg-gradient-to-t from-primary via-secondary to-transparent from-[0%] via-[10%] rounded-full"
+            className="absolute inset-x-0 top-0  w-[2px] bg-linear-to-t from-primary via-secondary to-transparent from-0% via-10% rounded-full"
           />
         </div>
       </div>
