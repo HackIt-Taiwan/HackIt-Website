608a052c2ede9a194c3e95461961e135ca7c578d
wolf-yuan <wolf@wolf-yuan.dev>
2025-02-01 15:00:49 +0800

    refactor: format some file

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,25 +3,24 @@ import Link from "next/link";
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-50 w-full p-y-2 p-x-5 bg-transparent">
+    <header className="p-y-2 p-x-5 sticky top-0 z-50 w-full bg-transparent">
       <div className="flex items-center justify-center">
         <div className="container flex h-14 items-center justify-between p-2">
           {/* 左側按鈕區域 */}
           <div className="flex space-x-1">
-          <Link href={"/"}>
-
-            <button className="p-1 bg-border hover:bg-primary">
-              <Icon icon="pixelarticons:home" width="18" height="18" />
-            </button>
+            <Link href={"/"}>
+              <button className="bg-border hover:bg-primary p-1">
+                <Icon icon="pixelarticons:home" width="18" height="18" />
+              </button>
             </Link>
           </div>
 
           {/* 右側按鈕區域 */}
           <div className="flex space-x-1">
             <Link href={"/news"}>
-            <button className="p-1 bg-border hover:bg-primary">
-              <p className="font-light text-sm">新聞與公告</p>
-            </button>
+              <button className="bg-border hover:bg-primary p-1">
+                <p className="text-sm font-light">新聞與公告</p>
+              </button>
             </Link>
           </div>
         </div>
